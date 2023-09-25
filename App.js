56bbdc8ae499d9815f33ee96b5a1d03acc247132
Tooga Garanja Magar <tugamagar77@gmail.com>
2023-09-25 13:27:15 -0400

    Export default function init

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { Button, Text, TextInput, StyleSheet, View } from "react-native";
+
+export default function BMI() {
+
+    const [value, onChangeText] = useState('Enter your weight');
+
+}
